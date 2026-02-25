c3f8d0ecf55835f839ed28e3e1ca0e417c000b06
Shivansh-Gupta-26 <shivansh.25b03110076@abes.ac.in>
2026-02-25 09:53:35 +0530

    javascript code

---
+++ script.js
@@ -0,0 +1,19 @@
+var num = 10;
+console.log(num);
+console.log(typeof(num));
+
+num="Shivansh";
+console.log(num);
+console.log(typeof(num));
+
+num=false;
+console.log(num);
+console.log(typeof(num));
+
+let num;
+console.log(num);
+console.log(typeof(num));
+
+num=null;
+console.log(num);
+console.log(typeof(num));
