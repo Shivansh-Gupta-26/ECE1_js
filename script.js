dfb216d159964b1349fb65bbbc4e6658d5133d8b
Shivansh-Gupta-26 <shivansh.25b03110076@abes.ac.in>
2026-03-11 09:51:20 +0530

    javascript

--- script.js
+++ script.js
@@ -1,31 +1,77 @@
-let num;
-console.log(num);
-console.log(typeof(num));
+// let num;
+// console.log(num);
+// console.log(typeof(num));
 
-num="Shivansh";
-console.log(num);
-console.log(typeof(num));
+// num="Shivansh";
+// console.log(num);
+// console.log(typeof(num));
 
-num=false;
-console.log(num);
-console.log(typeof(num));
+// num=false;
+// console.log(num);
+// console.log(typeof(num));
 
-num=10;
-console.log(num);
-console.log(typeof(num));
+// num=10;
+// console.log(num);
+// console.log(typeof(num));
 
-num=null;
-console.log(num);
-console.log(typeof(num));
+// num=null;
+// console.log(num);
+// console.log(typeof(num));
 
 
-const sym1 =Symbol(4)
-console.log(sym1);
-const sym2=Symbol(4);
-console.log(sym2);
-if(sym1==sym2){
-    console.log("true");
-}
-else{
-    console.log("false");
+// const sym1 =Symbol(4)
+// console.log(sym1);
+// const sym2=Symbol(4);
+// console.log(sym2);
+// if(sym1==sym2){
+//     console.log("true");
+// }
+// else{
+//     console.log("false");
+// }
+
+// let count;
+// document.write("string Loop"+"<br/>")
+// for(count=0;count<10;count++){
+//     document.write("Current Count:"+count);
+//     document.write("<br/>");
+// }
+
+// let x;
+
+//     let person{
+//     fname:"Ajay";
+//     lname:"Singh";
+//     age:25;
+//     }
+//     for(x in person){
+//         console.log("personal details:"+ x +":"+person[x])
+//     }
+
+// let count = 0;
+// while (count < 10) {
+
+// }
+
+
+// conditional statement
+// let age=17;
+// if(age>18){
+//     document.write("<b>You are eligible to vote.</b>")
+// }
+// else{
+//     document.write("<b>You are not eligible to vote.</b>")
+// }
+
+
+let grade='A';
+switch(grade){
+    case'A':document.write("<br>Godd job<br/>");
+    break;
+        case'B':document.write("<br>Godd job<br/>");
+    break;
+        case'C':document.write("<br>Godd job<br/>");
+    break;
+        default:document.write("<br>Godd job<br/>");
+    break;
 }
