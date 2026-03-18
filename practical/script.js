3c02f851aa957776854fcf64a1b903e82da703ad
Shivansh-Gupta-26 <shivansh.25b03110076@abes.ac.in>
2026-03-18 10:24:35 +0530

    practical javascript

--- practical/script.js
+++ practical/script.js
@@ -1,29 +1,34 @@
-function calculateResult(){
-let n=document.getElementById("subjects").value;
-let i;
-let total=0;
-for(i=0;i<n;i++){
-    let x=parseFloat(prompt("Enter The Subject No."+(i+1)));
-     total += x;
-}
-let average=total/n;
-let grade;
-if(average>=90){
-    grade = "A";
-}
-else if(average>=75){
-    grade = "B";
-}
-else if(average>=60){
-    grade = "C";
-}
-    else if(average>=40){
-    grade = "D";
-    }
-    else{
-    grade = "F";
-    }
+// function calculateResult(){
+// let n=document.getElementById("subjects").value;
+// let i;
+// let total=0;
+// for(i=0;i<n;i++){
+//     let x=parseFloat(prompt("Enter The Subject No."+(i+1)));
+//      total += x;
+// }
+// let average=total/n;
+// let grade;
+// if(average>=90){
+//     grade = "A";
+// }
+// else if(average>=75){
+//     grade = "B";
+// }
+// else if(average>=60){
+//     grade = "C";
+// }
+//     else if(average>=40){
+//     grade = "D";
+//     }
+//     else{
+//     grade = "F";
+//     }
 
-      let r=document.getElementById("result").innerHTML="Total Marks"+total+"<br>Average Marks: "+average+"<br>Grade: "+grade;
+//       let r=document.getElementById("result").innerHTML="Total Marks"+total+"<br>Average Marks: "+average+"<br>Grade: "+grade;
 
-}
+// }
+
+// array
+var myinfo=new array();
+var myinfo1=array();
+console.log(myinfo,myinfo1);
