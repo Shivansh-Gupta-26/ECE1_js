23c37ad08b9d7b58b55e14048e6ece2ff2341c99
Shivansh-Gupta-26 <shivansh.25b03110076@abes.ac.in>
2026-03-18 10:23:52 +0530

    document javascript

---
+++ Dom/script.js
@@ -0,0 +1,59 @@
+// const result = document.getElementById("jk");
+//  console.log(result);
+// result.innerHTML="<b> HelloWorld </b>";
+
+// const result1= document.getElementsById("p1");
+// result1[1].innerHTML="chauhan";
+// result1[0].innerHTML="Jitendra";
+
+//  const result = document.getElementById("jk").getAttribute("class");
+//  document.getElementById("p1").innerHTML ="jk1";
+
+//  const heading=document.createElement("h2");
+
+function onClick(){
+    console.log("on Click");
+}
+function onmouseover(){
+    console.log("onmouseover");
+}
+function onmouseout(){
+    console.log("onmouseout");
+}
+function onmousedown(){
+    console.log("onmousedown");
+}
+function ondblclick(){
+    console.log("ondblclick");
+}
+function onmouseup(){
+    console.log("on onmouseup");
+}
+function onmousemove(){
+    console.log("onmousemove");
+}
+// append
+// prepend
+// before 
+// after
+
+
+// onclick
+// onmouseover
+// onmouseout
+// onmousedown
+// onmouseup
+// onmousemove
+// ondblclick
+
+// 
+// onfocus
+// onsubmit
+// 
+// 
+// 
+// 
+// onload
+// onunload
+// onresize
+// 
