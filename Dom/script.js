e84277de8f15c03a5b48f5fdc6970ba3d0f868cf
Shivansh-Gupta-26 <shivansh.25b03110076@abes.ac.in>
2026-04-11 13:50:04 +0530

    javascript

--- Dom/script.js
+++ Dom/script.js
@@ -11,27 +11,27 @@
 
 //  const heading=document.createElement("h2");
 
-function onClick(){
-    console.log("on Click");
-}
-function onmouseover(){
-    console.log("onmouseover");
-}
-function onmouseout(){
-    console.log("onmouseout");
-}
-function onmousedown(){
-    console.log("onmousedown");
-}
-function ondblclick(){
-    console.log("ondblclick");
-}
-function onmouseup(){
-    console.log("on onmouseup");
-}
-function onmousemove(){
-    console.log("onmousemove");
-}
+// function onClick(){
+//     console.log("on Click");
+// }
+// function onmouseover(){
+//     console.log("onmouseover");
+// }
+// function onmouseout(){
+//     console.log("onmouseout");
+// }
+// function onmousedown(){
+//     console.log("onmousedown");
+// }
+// function ondblclick(){
+//     console.log("ondblclick");
+// }
+// function onmouseup(){
+//     console.log("on onmouseup");
+// }
+// function onmousemove(){
+//     console.log("onmousemove");
+// }
 // append
 // prepend
 // before 
@@ -57,3 +57,33 @@ function onmousemove(){
 // onunload
 // onresize
 // 
+
+// let promise = new Promise((resolve,reject)=>{
+// let success = true;
+// if(success)
+//     resolve("Data Loaded");
+// else
+//     reject("Error Occured");
+// });
+// promise.then(result => console.log(result)).catch(error => console.log(error));
+
+// async function getData(){
+//     let reponse = await fetch("https://jsonplaceholder.typicode.com/posts");
+//     let data = await response.json();
+//     console.log(data);
+// }
+// getData();
+
+// Fetch API
+// fetch("https://jsonplaceholder.typicode.com/posts").then
+
+// Error Handling
+// try{
+
+// }
+
+// localStorage.setItem() 
+
+
+var user = {name:"Jeet",age:50};
+localStorage
