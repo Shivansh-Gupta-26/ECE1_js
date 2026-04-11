976a6b45acaa27a6867eb750cdc18bc633c430bb
Shivansh-Gupta-26 <shivansh.25b03110076@abes.ac.in>
2026-04-11 13:50:23 +0530

    javascript

--- practical/script.js
+++ practical/script.js
@@ -1,34 +1,30 @@
-// function calculateResult(){
-// let n=document.getElementById("subjects").value;
-// let i;
-// let total=0;
-// for(i=0;i<n;i++){
-//     let x=parseFloat(prompt("Enter The Subject No."+(i+1)));
-//      total += x;
-// }
-// let average=total/n;
-// let grade;
-// if(average>=90){
-//     grade = "A";
-// }
-// else if(average>=75){
-//     grade = "B";
-// }
-// else if(average>=60){
-//     grade = "C";
-// }
-//     else if(average>=40){
-//     grade = "D";
-//     }
-//     else{
-//     grade = "F";
-//     }
+function calculateResult(){
+    let n = document.getElementById("subject").value;
+    let i ;
+    let total=0;
+    for(i=0;i<n;i++){
+        let x = (parseFloat(prompt("Enter the subject No. " + (i+1))));
+        total += x;
+    }
+        let average = total/n;
+        let grade;
+        if(average >= 75){
+            grade = "A";
+        }else if(average >= 60){
+            grade = "B";
+        }else if(average >= 50){
+            grade = "C";
+        }else if(average >= 40){
+            grade = "D";
+        }else{
+            grade = "F";
+        } 
+        let Result;
+        if(average >= 40){
+            result= "Pass";
+        }else{
+            result = "Fail";
+        }
 
-//       let r=document.getElementById("result").innerHTML="Total Marks"+total+"<br>Average Marks: "+average+"<br>Grade: "+grade;
-
-// }
-
-// array
-var myinfo=new array();
-var myinfo1=array();
-console.log(myinfo,myinfo1);
+   let res = document.getElementById("result").innerHTML = "Total Marks: " + total + "<br/>" + "Average Marks: " + average + "<br/>" + "Grade: " + grade + "<br/>" + "Result: "+ result + "<br/>";
+}
